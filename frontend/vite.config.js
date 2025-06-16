7f7cd9b0bf9bdd0877b54f77f808fcdd452bd49f
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-06-16 05:51:03 +0100

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,7 +5,15 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  },
+  server: {
+    open: true
   },
   define: {
     'process.env': {}
