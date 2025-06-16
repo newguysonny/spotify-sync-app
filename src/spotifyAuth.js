98f8b57b1ded0cc60ab91c6f2bd4d70ce3c890be
newguysonny <128412615+newguysonny@users.noreply.github.com>
2025-06-16 02:55:35 +0100

    Update spotifyAuth.js

--- src/spotifyAuth.js
+++ src/spotifyAuth.js
@@ -1,19 +1,20 @@
-const axios = require('axios');
-const qs = require('querystring');
+import axios from 'axios';
+import { stringify } from 'querystring';
 
 async function exchangeCodeForTokens(code) {
   const resp = await axios.post(
     'https://accounts.spotify.com/api/token',
-    qs.stringify({
+    stringify({
       grant_type: 'authorization_code',
       code,
-      redirect_uri: process.env.REDIRECT_URI,
+      redirect_uri: process.env.REDIRECT_URL,
       client_id: process.env.SPOTIFY_CLIENT_ID,
       client_secret: process.env.SPOTIFY_CLIENT_SECRET
     }),
     { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
   );
-  return resp.data; // includes access_token, refresh_token, expires_in
+  return resp.data; // { access_token, refresh_token, expires_in }
 }
 
-module.exports = { exchangeCodeForTokens };
+// Named export (ESM alternative to module.exports)
+export { exchangeCodeForTokens };
